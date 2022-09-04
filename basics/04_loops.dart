8d9b330578884e9f0cb09c811cd70efe410ef8d9
Vitor Silva <vitor.batista@quintoandar.com.br>
2022-09-04 09:54:46 -0300

    Adding loops examples

---
+++ basics/04_loops.dart
@@ -0,0 +1,13 @@
+void main() {
+  //FOR: will exececute a block of code a specific number of times.
+  for (int i = 0; i <= 10; i++) {
+    print('For print: $i'); //wil print 0 to 10
+  }
+
+  //WHILE: will execute a block of code while a condition is true.
+  int counter = 10;
+  while (counter != 1) {
+    counter = counter - 1;
+    print('While counter: $counter'); //wil print 0 to 10
+  }
+}
